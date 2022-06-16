b09d36fa81ea717b67f476cb198ec5bebf5e2652
diegofdg <diegofdg@gmail.com>
2022-06-16 04:26:11 -0300

    clase_07-ordenando_listas

--- bytebank/src/com/bytebank/modelo/Cuenta.java
+++ bytebank/src/com/bytebank/modelo/Cuenta.java
@@ -7,7 +7,7 @@ package com.bytebank.modelo;
  * @author diego
  *
  */
-public abstract class Cuenta {
+public abstract class Cuenta implements Comparable<Cuenta> {
 	
 	protected double saldo;
     private int agencia;
@@ -102,7 +102,8 @@ public abstract class Cuenta {
     
     @Override
     public String toString() {
-    	String cuenta = "Numero: " + this.numero + ", Agencia:" + this.agencia;
+    	String cuenta = "Numero: " + this.numero + ", Agencia:" + this.agencia
+    			+ ", Titular: " + this.titular.getNombre();
     	return cuenta;
     }
     
@@ -112,5 +113,13 @@ public abstract class Cuenta {
     	Cuenta cuenta = (Cuenta) obj;
     	return this.agencia == cuenta.getAgencia() && 
     			this.numero == cuenta.getNumero();
+    }
+    
+    @Override
+    public int compareTo(Cuenta o) {
+    	// Orden natural: Numero Agencia
+    	//return Integer.compare(this.agencia, o.getAgencia());
+    	// Orden natural: Saldo
+    	return Double.compare(this.getSaldo(), o.getSaldo());
     }
 }
